10cc521b5be56890e016e188a3b634d5829a27d6
Boi Schrader <boi1992@t-online.de>
2022-07-04 12:23:11 +0200

    almost all done, problems with nr 8

--- solution.js
+++ solution.js
@@ -1,1 +1,56 @@
+console.log('-------------------------');
+const parWalkstr = "I can walk in the park all day!";
+console.log (parWalkstr.slice(-13,22));
+
+console.log('-------------------------');
+const helloStr = "I can walk in the park all day!"
+console.log(helloStr.toUpperCase())
+
+console.log('-------------------------');
+const earthlingsStr = "Hello Earthling"
+console.log(earthlingsStr.toLowerCase())
+
+console.log('-------------------------');
+const jsStr = "JavaScript"
+console.log(jsStr.slice(-7,6))
+
+console.log('-------------------------');
+const niceShoesStr = "nice shoes"
+
+const letterL = 'Ll';
+const letterN = 'n';
+
+console.log(`The letter "${letterL}" ${niceShoesStr.includes(letterL) ? 'is' : 'is not'} in the sentence`);
+console.log(`The letter "${letterN}" ${niceShoesStr.includes(letterN) ? 'is' : 'is not'} in the sentence`);
+
+console.log('-------------------------');
+const oldStr = "bubbles"
+const newStr = oldStr
+console.log(oldStr.slice(0,1) + newStr)
+
+console.log('-------------------------');
+const scritherStr = "Scritch"
+const lastThreeChars = scritherStr.slice(-3)
+console.log(lastThreeChars + scritherStr + lastThreeChars)
+
+console.log('-------------------------');
+const boogieNightsStr = 'BoogieWoogie'
+
+console.log('-------------------------');
+
+const firstName = 'Maria';
+const city = 'Berlin';
+const job = 'teacher';
+
+console.log('My name is', firstName ,'. I live in' , city, 'and I am a', job)
+
+console.log('-------------------------');
+const foxStr = 'the quick brown fox'
+
+const firstLetter = foxStr[0].toUpperCase()
+const restLetters = foxStr.substring(1).toLowerCase()
+
+console.log (firstLetter + restLetters)
+
+console.log('-------------------------');
 
